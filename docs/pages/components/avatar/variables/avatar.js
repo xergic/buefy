2d051895210546115206ee2353d558d9d34529f9
Ondra Kandera <xergic@gmail.com>
2021-05-19 10:47:41 +0200

    Adds border variable for Avatar

--- docs/pages/components/avatar/variables/avatar.js
+++ docs/pages/components/avatar/variables/avatar.js
@@ -8,6 +8,10 @@ export default [
         default: '<code>findLightColor($primary)</code>'
     },
     {
+        name: '<code>$avatar-border</code>',
+        default: '<code>none</code>'
+    },
+    {
         name: 'Bulma variables',
         default: '<a target="_blank" href="https://bulma.io/documentation/elements/image/#variables">Link</a>'
     }
